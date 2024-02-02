8466fdd24784e688fe5e46edc92af14d23754e0b
Mouhcine Essafi <messafi1337@gmail.com>
2024-02-02 17:27:43 +0100

    edit the front
    edit the front

--- e-commerce/resources/views/products/index.blade.php
+++ e-commerce/resources/views/products/index.blade.php
@@ -1,51 +1,63 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>liste des produits</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-    <h1>liste des produits</h1>
-    <div class="container text-center">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container-xl">
         <div class="row">
-            @if (session('status'))
-                <div class="alert alert-success">
-                    {{session('status')}}
-                </div>
-            @endif
-            <div class="col s12">
-                <hr>
-                <a href="{{url('/products.create')}}" class="btn btn-primary">ajouter un produit</a>
-                <hr>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>name</th>
-                            <th>description</th>
-                            <th>price</th>
-                            <th>quantity</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($produits as $produit)
-                        <tr>
-                            <td> {{ $produit->name }} </td>
-                            <td> {{ $produit->description }} </td>
-                            <td> {{ $produit->price }} </td>
-                            <td> {{ $produit->quantity }} </td>
-                            <td>
-                                <a href="/products/edit/ {{ $produit->id }} " class="btn btn-info">Update</a>
-                                <a href="/products/delete/ {{ $produit->id }}" class="btn btn-danger">Delete</a>
-                            </td>
-                        </tr>
+            <div class="col-md-12">
+                <h2>ALL <b>Products</b></h2>
+                <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
+                    <!-- Carousel indicators -->
+                    <ol class="carousel-indicators" id="carousel-indicators"></ol>
+                    <!-- Wrapper for carousel items -->
+                    <div class="carousel-inner" id="carousel-inner">
+                        @foreach ($produits->chunk(4) as $chunk)
+                            <div class="item carousel-item {{ $loop->first ? 'active' : '' }}">
+                                <div class="row">
+                                    @foreach ($chunk as $produit)
+                                        <div class="col-sm-3">
+                                            <div class="thumb-wrapper">
+                                                <div class="img-box">
+                                                    <img src="{{ $produit->image_path }}" class="img-fluid" alt="{{ $produit->name }}">
+                                                </div>
+                                                <div class="thumb-content">
+                                                    <h4>{{ $produit->name }}</h4>
+                                                    <p class="item-price">
+                                                        {{-- <strike>${{ $produit->price }}</strike> --}}
+                                                        <b>{{ $produit->price }} DH</b>
+                                                    </p>
+                                                    <a href="#" class="btn btn-primary">Details</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                </div>
+                            </div>
                         @endforeach
-                    </tbody>
-                </table>
+                    </div>
+                    <!-- Carousel controls -->
+                    <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
+                        <i class="fa fa-angle-left"></i>
+                    </a>
+                    <a class="carousel-control-next" href="#myCarousel" data-slide="next">
+                        <i class="fa fa-angle-right"></i>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
